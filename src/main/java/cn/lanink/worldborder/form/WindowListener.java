189c0927f8fb5202335545d68c53c6fba135f9f7
lt_name <572413378@qq.com>
2021-02-08 20:38:20 +0800

    Stuff

--- src/main/java/cn/lanink/worldborder/form/WindowListener.java
+++ src/main/java/cn/lanink/worldborder/form/WindowListener.java
@@ -15,23 +15,24 @@ public class WindowListener implements Listener {
         if (player == null || event.getWindow() == null || event.getResponse() == null) {
             return;
         }
-        if (event.getWindow() instanceof FormWindowSimple) {
-            if (event.getFormID() == WindowCreate.ADMIN_MENU) {
-                FormWindowSimple simple = (FormWindowSimple) event.getWindow();
-                switch (simple.getResponse().getClickedButtonId()) {
-                    case 0:
-                        Server.getInstance().dispatchCommand(player, "worldborder pos1");
-                        break;
-                    case 1:
-                        Server.getInstance().dispatchCommand(player, "worldborder pos2");
-                        break;
-                    case 2:
-                        Server.getInstance().dispatchCommand(player, "worldborder delete");
-                        break;
-                    case 3:
-                        Server.getInstance().dispatchCommand(player, "worldborder reload");
-                        break;
-                }
+        if (event.getWindow() instanceof FormWindowSimple &&
+                event.getFormID() == WindowCreate.ADMIN_MENU) {
+            FormWindowSimple simple = (FormWindowSimple) event.getWindow();
+            switch (simple.getResponse().getClickedButtonId()) {
+                case 0:
+                    Server.getInstance().dispatchCommand(player, "worldborder pos1");
+                    break;
+                case 1:
+                    Server.getInstance().dispatchCommand(player, "worldborder pos2");
+                    break;
+                case 2:
+                    Server.getInstance().dispatchCommand(player, "worldborder delete");
+                    break;
+                case 3:
+                    Server.getInstance().dispatchCommand(player, "worldborder reload");
+                    break;
+                default:
+                    break;
             }
         }
     }
