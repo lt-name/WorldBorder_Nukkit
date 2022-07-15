2256c92b24120fb17e640c95ca0d6c3d5519823d
LT_Name <572413378@qq.com>
2022-07-15 20:02:06 +0800

    添加worldborder.leave权限

--- src/main/java/cn/lanink/worldborder/border/Border.java
+++ src/main/java/cn/lanink/worldborder/border/Border.java
@@ -116,7 +116,8 @@ public class Border implements Cloneable {
      */
     public boolean canLeave(Player player) {
         //TODO 完善这个判断
-        return player.isOp() && WorldBorder.getInstance().getConfig().getBoolean("op-can-leave");
+        return player.isOp() && WorldBorder.getInstance().getConfig().getBoolean("op-can-leave") ||
+                player.hasPermission("worldborder.leave");
     }
 
     /**
